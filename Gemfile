b4db4acd3c7038ba79d806621cff9322888a2ae0
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2014-05-04 17:40:41 +0300

    simplecov gem for coverage introduced

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :test do
   gem "capybara"
   gem 'selenium-webdriver'
   gem "launchy"
+  gem 'simplecov', '~> 0.7.1', :require => false
 end
 
 group :production do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails' if ENV['COVERAGE'] == 'yes'
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
