d39b29a4e8cb81e7a59591d4796098254b2d0541
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2014-05-04 14:31:10 +0300

    Mutant and Task numbers on Group index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,23 @@ module ApplicationHelper
     end
   end
 
-  def number_of_mutants(task)
-    if task.mutants.count == 1
+  def number_of_mutants(task_or_group)
+    if task_or_group.mutants.count == 1
       '1 Mutant'
-    elsif task.mutants.count > 1
-      "#{task.mutants.count} Mutants"
+    elsif task_or_group.mutants.count > 1
+      "#{task_or_group.mutants.count} Mutants"
     else
       'No Mutant'
     end
+  end
+
+  def number_of_tasks(group)
+    if group.tasks.count == 1
+      '1 Task'
+    elsif group.tasks.count > 1
+      "#{group.tasks.count} Tasks"
+    else
+      'No Task'
+    end    
   end
 end
