5acb8b6e97b7f57d988d8f25c7da8c13d46a8d21
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2014-05-10 08:31:29 +0300

    Scenario: I want to be able to create a Task from within Group New page without leaving Group New page

--- features/step_definitions/group_management.rb
+++ features/step_definitions/group_management.rb
@@ -253,11 +253,16 @@ And(/^I can save the new Task$/) do
   @page.save.click
 end
 
-And(/^I can see the new Task as selected in the list of available Tasks on Group edit page$/) do
+And(/^I can see the new Task as selected in the list of available Tasks on Group page$/) do
   expect(@page.selected_task_names).to include(@new_task_name)
 end
 
 Then(/^The new Task is associated to the particular Group$/) do
   task = Mutants::Task.find_by_name(@new_task_name)
   expect(task.group.id).to eq(@group.id)
+end
+
+And(/^I click on the Create button$/) do
+  @page.create_button.click
+  @group = Mutants::Group.last
 end
